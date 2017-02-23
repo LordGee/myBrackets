cd04029b879fc37ddec087cc15da97bf45a10f0f
LordGee <gordon.johnson@mychaos.co.uk>
2017-02-23 01:04:40 +0000

    Re-organised file structure
    + Brought in elements that are good from node and asp projects

---
+++ PHP/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Muli" rel="stylesheet">
+    <link href="style/elements.css" rel="stylesheet" type="text/css" />
+    <link href="style/main.css" rel="stylesheet" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <title>myBrackets</title>
+</head>
+<body>
+<div id="sideBar">
+    <nav>
+        <a class="navLink" href="index.php">Go Home</a>
+        <a class="navLink" href="app/SignUp">Sign-up here</a>
+    </nav>
+</div>
+<div id="mainArea">
+    <div id="headerArea">
+        <div id="menuButton"><button id="menuToggle" class="button">Hide Menu</button></div>
+        <img src="image/myBrackets Logo 400w200h.png" alt="myBrackets Logo" title="myBrackets" id="logoImage" />
+    </div>
+    <div>
+
+        <h1>This is a header</h1>
+        <p>This is standard text, hope you enjoy it</p>
+
+
+
+    </div>
+</div>
+
+<script src="js/navigation.js" type="text/javascript"></script>
+
+</body>
+</html>
---
+++ PHP/js/navigation.js
@@ -0,0 +1,13 @@
+﻿﻿
+(function () {
+    var $sidebarAndWrapper = $("#sideBar,#mainArea,#menuButton");
+    $("#menuToggle").on("click",
+        function () {
+            $sidebarAndWrapper.toggleClass("hide-sidebar");
+            if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+                $(this).text("Show Menu");
+            } else {
+                $(this).text("Hide Menu");
+            }
+        });
+})();
