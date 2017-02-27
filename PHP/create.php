79595110ed148e5498c182f59750eaa3407a42dc
LordGee <gordon.johnson@mychaos.co.uk>
2017-02-27 23:46:28 +0000

    Minor additions to the create event page

--- PHP/create.php
+++ PHP/create.php
@@ -4,6 +4,7 @@
 <div id="content">
     <h1>Create New Event</h1>
     <div class="contentArea">
+
         <h2>Step 1 - Basic Details</h2>
         <form method="post" action="create.php">
             <div class="inputItem">
@@ -18,6 +19,21 @@
                 <div class="inputWrap">
                     <span class="inputIcon"><i class="fa fa-pencil-square-o fa-fw fa-lg" aria-hidden="true"></i></span>
                     <textarea name="e_description" placeholder="Describe your event" rows="10"></textarea>
+                </div>
+            </div>
+            <div class="inputItem">
+                <input type="hidden" name="code" value="addBasic">
+                <input type="submit" value="Add Players">
+            </div>
+        </form>
+        <br/>
+
+        <h2>Add Players</h2>
+        <form method="post" action="create.php">
+            <div class="inputItem">
+                <div class="inputWrap">
+                    <span class="inputIcon inputParticipant"><i class="fa fa-user fa-fw fa-lg" aria-hidden="true"></i> 1</span>
+                    <input type="text" name="e_name[]" placeholder="Enter the participants name">
                 </div>
             </div>
         </form>
